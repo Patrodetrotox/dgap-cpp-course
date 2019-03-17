2072bad2d281752a043e6a09aa696cdf9df841dc
Sergey <48199979+Patrodetrotox@users.noreply.github.com>
2019-03-17 21:53:02 +0300

    Update queue.h

--- task02/queue.h
+++ task02/queue.h
@@ -1,46 +1,72 @@
-
 #pragma once
+
+#include <string>
 
 typedef int data;
 
-class BasicQueue
-{
-public:
-
-    // Add element to the end
-    virtual void enqueue(data &) = 0;
-
-    // Return first element
-    virtual data dequeue() = 0;
-
-    // Get total count of elements
-    virtual unsigned getLength() = 0;
-
+struct Node {
+	Node();
+	Node(const data& a, Node* l = NULL, Node* r = NULL);
+	data val;
+	Node* lhs;
+	Node* rhs;
 };
 
-class ListQueue: public BasicQueue
-{
+class BasicQueue {
 public:
-    ListQueue();
 
-    /*
-     * Fully implement class:
-     * - declarations here
-     * - definitions in queue.cpp
-     * Could have infinite size
-     */
+	// Add element to the end
+	virtual void enqueue(const data& a) = 0;
+
+	// Return first element
+	virtual data dequeue() = 0;
+
+	// Get total count of elements
+	virtual unsigned getLength() = 0;
+protected:
+	unsigned length;
 };
 
-class ArrayQueue: public BasicQueue
-{
+class ListQueue: public BasicQueue {
 public:
-    ArrayQueue(unsigned max_size = 64);
+	ListQueue();
 
-    /*
-     * Fully implement class:
-     * - declarations here
-     * - definitions in queue.cpp
-     * Only finite size implementation is required
-     */
+	void enqueue(const data& a) override;
+
+	data dequeue() override;
+
+	unsigned getLength() override;
+
+	/*
+	* Fully implement class:
+	* - declarations here
+	* - definitions in queue.cpp
+	* Could have infinite size
+	*/
+
+private:
+	Node* head;
+	Node* last;
 };
 
+class ArrayQueue: public BasicQueue {
+public:
+	ArrayQueue(unsigned max_size = 64);
+
+	void enqueue(const data& a) override;
+
+	data dequeue() override;
+
+	unsigned getLength() override;
+
+	/*
+	* Fully implement class:
+	* - declarations here
+	* - definitions in queue.cpp
+	* Only finite size implementation is required
+	*/
+private:
+	unsigned size;
+	unsigned current;
+	data* q;
+};
