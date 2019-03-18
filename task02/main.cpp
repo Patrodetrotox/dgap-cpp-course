e89e0dfd3baad54618b64d59add6fde140b62a4b
Sergey <serg.tikhonoff@yandex.ru>
2019-03-18 20:15:30 +0300

    -

--- task02/main.cpp
+++ task02/main.cpp
@@ -4,10 +4,61 @@
 using namespace std;
 
 int main() {
-	ListQueue q;
-	q.enqueue(5);
-	q.enqueue(4);
-	q.enqueue(3);
-	cout << q.dequeue() << " " << q.dequeue() << " " << q.dequeue() << " " << q.dequeue() << endl;
+
+	//ListBased tests
+	ListQueue<int> q1;
+	ListQueue<string> q2;
+	q1.enqueue(5);
+	q1.enqueue(4);
+	q1.enqueue(3);
+	try {
+		cout << q1.dequeue() << endl;
+	} catch (range_error& er) {
+		cerr << "Error: " << er.what() << endl;
+	}
+
+	q2.enqueue("Hello ");
+	q2.enqueue("This ");
+	q2.enqueue("World!");
+	try {
+		cout << q2.dequeue() << q2.dequeue() << q2.dequeue() << endl;
+		cout << q2.dequeue() << endl;
+	} catch(range_error& er) {
+		cerr << "Error: " << er.what() << endl;
+	}
+
+	//ArrayBased tests
+
+	ArrayQueue<int, 4> qq1;
+	ArrayQueue<int> qq2;
+
+	try {
+		qq1.enqueue(1);
+		qq1.enqueue(2);
+		cout << qq1.getLength() << endl;
+		qq1.enqueue(3);
+		qq1.enqueue(4);
+		qq1.enqueue(5);
+
+	} catch(range_error& er) {
+		cerr << "Error: " << er.what() << endl;
+	}
+
+	try {
+		cout << qq1.dequeue() << " " << qq1.dequeue() << " " << qq1.dequeue() << " " << qq1.dequeue() << endl;
+		cout << qq1.dequeue() << endl;
+	} catch (range_error& er) {
+		cerr << "Error: " << er.what() << endl;
+	}
+
+	//getLength test
+
+	qq2.enqueue(1);
+	qq2.enqueue(2);
+	qq2.dequeue();
+	qq2.dequeue(); // first = 2
+	qq2.enqueue(3);
+	qq2.enqueue(4); // last = 0
+	cout << qq2.getLength() << endl;  //should return 2
     return 0;
 }
--- task02/queue.h
+++ task02/queue.h
@@ -1,17 +1,29 @@
 #pragma once
 
 #include <string>
+#include<exception>
 
-typedef int data;
-
+template <typename data>
 struct Node {
-	Node();
-	Node(const data& a, Node* l = NULL, Node* r = NULL);
+
+	Node():
+		val(0),
+		rhs(NULL),
+		lhs(NULL) {
+	}
+
+	Node(const data& a, Node* l = NULL, Node* r = NULL):
+		val(a),
+		lhs(l),
+		rhs(r) {
+	}
+
 	data val;
-	Node* lhs;
-	Node* rhs;
+	Node<data>* lhs;
+	Node<data>* rhs;
 };
 
+template <typename data>
 class BasicQueue {
 public:
 
@@ -27,46 +39,135 @@ protected:
 	unsigned length;
 };
 
-class ListQueue: public BasicQueue {
+template <typename data>
+class ListQueue: public BasicQueue<data> {
 public:
-	ListQueue();
+
+	ListQueue():
+		head(NULL),
+		last(NULL),
+		length(0) {
+	}
+
+	~ListQueue() {
+		Node<data>* buf;
+		while( last != NULL) {
+			buf = last->lhs;
+			delete last;
+			last = buf;
+		}
+	}
 
 	void enqueue(const data& a) override;
 
 	data dequeue() override;
 
-	unsigned getLength() override;
-
-	/*
-	* Fully implement class:
-	* - declarations here
-	* - definitions in queue.cpp
-	* Could have infinite size
-	*/
+	unsigned getLength() override {
+		return length;
+	}
 
 private:
-	Node* head;
-	Node* last;
+	Node<data>* head;  //first to leave
+	Node<data>* last;  // last to leave (tail)
+	unsigned length;
 };
 
-class ArrayQueue: public BasicQueue {
+template <typename data, int N = 64>
+class ArrayQueue: public BasicQueue<data> {
 public:
-	ArrayQueue(unsigned max_size = 64);
+	ArrayQueue():
+		size(N),
+		first(0),
+		last(0),
+		is_empty(true) {
+	}
 
 	void enqueue(const data& a) override;
 
 	data dequeue() override;
 
-	unsigned getLength() override;
+	unsigned getLength() override {
+		if( last >= first) {
+			return last - first + 1;
+		} else {
+			return (N - first + 1 + last);   // (N - first) elements before N and (last + 1) elements before last (including 0)
+		}
+	}
 
-	/*
-	* Fully implement class:
-	* - declarations here
-	* - definitions in queue.cpp
-	* Only finite size implementation is required
-	*/
 private:
 	unsigned size;
-	unsigned current;
-	data* q;
+	unsigned first;    //number of the first to leave
+	unsigned last;     //number of the last in queue
+	bool is_empty;
+	data q[N];
 };
+
+// bigger than usual definitions from here
+
+template <typename data>
+void ListQueue<data>::enqueue(const data& a) {
+	Node<data>* New = new Node<data>(a, last);
+	if(head == NULL) {
+		head = New;
+		last = New;
+	} else {
+		last->rhs = New;
+		last = New;
+	}
+	length++;
+}
+
+template <typename data>
+data ListQueue<data>::dequeue() {
+
+	if(length == 0u) {
+		throw std::range_error("Empty queue!");
+	}
+
+	Node<data> buf = *head;
+	if(length == 1u) {
+		delete head;
+		head = NULL;
+		last = NULL;
+	} else {
+		delete head;
+		head = buf.rhs;
+		head->lhs = NULL;
+	}
+	length--;
+	return buf.val;
+}
+
+template <typename data, int N>
+void ArrayQueue<data, N>::enqueue(const data& a) {
+
+	if(is_empty) {
+		is_empty = false;
+		q[first] = a;
+		q[last] = a;
+	} else {
+		if(first == (last + 1) % N) {
+			throw std::range_error("Queue is already full!");
+		}
+
+		last = (last + 1) % N;
+		q[last] = a;
+	}
+}
+
+template <typename data, int N>
+data ArrayQueue<data, N>::dequeue() {
+
+	if(is_empty) {
+		throw std::range_error("Queue is empty!");
+	}
+
+	data buf;
+	buf = q[first];
+	if(first == last) {
+		is_empty = true;
+	} else {
+		first = (first + 1) % N;
+	}
+	return buf;
+}
