356e1fd7ceb6e6c361fd559b2f75e32057df4b06
Sergey <serg.tikhonoff@yandex.ru>
2019-05-12 02:13:53 +0300

    task 4

--- task04/main.cpp
+++ task04/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include "storage.h"
@@ -20,11 +19,22 @@ int main()
     // But with usual char* it doesn't work:
     char str[100] = "Hello";
     Storage<char*> st_str(str);
-    cout << "Stored str " << st_int.GetVal() << endl;
+    cout << "Stored str " << st_str.GetVal() << endl;
     str[1] = 'X';
     // Value in storage broken:
-    cout << "Stored str " << st_int.GetVal() << endl;
+    cout << "Stored str " << st_str.GetVal() << endl;
 
+    Storage<char*> s;
+    try{
+    	cout << s.GetVal() << endl;
+    } catch(range_error& er) {
+    	cerr << er.what() << endl;
+    }
+    s.SetVal("Hello World!");
+    cout << s.GetVal() << endl;
+    s.SetVal("Hello");
+    cout << s.GetVal() << endl;
+    s.SetVal("Hello World!");
+    cout << s.GetVal() << endl;
     return 0;
 }
-
--- task04/storage.h
+++ task04/storage.h
@@ -1,4 +1,4 @@
-
+#include<cstring>
 
 template <typename T>
 class Storage
@@ -6,7 +6,9 @@ class Storage
 public:
     Storage() = default;
     Storage(const T &val);
-    ~Storage();
+    ~Storage() {
+
+    }
 
     void SetVal(const T &val);
     T GetVal();
@@ -14,3 +16,60 @@ private:
     T m_val;
 };
 
+template <>
+class Storage <char*> {
+public:
+	Storage() {
+		m_val = 0;
+	}
+	Storage(const char* val);
+	~Storage();
+
+	void SetVal(const char* val);
+	char* GetVal();
+private:
+	char* m_val;
+};
+
+template <typename T>
+Storage<T>::Storage(const T& val) {
+	m_val = val;
+}
+
+template <typename T>
+void Storage<T>::SetVal(const T& val) {
+	m_val = val;
+}
+
+template <typename T>
+T Storage<T>::GetVal() {
+	return m_val;
+}
+
+Storage<char*>::Storage(const char* val) {
+	m_val = new char[strlen(val) + 1];
+	std::strcpy(m_val, val);
+}
+
+Storage<char*>::~Storage() {
+	if(m_val != 0) {
+		delete[] m_val;
+	}
+}
+
+void Storage<char*>::SetVal(const char* val) {
+	if(m_val != 0) {
+		delete[] m_val;
+	}
+	m_val = new char[strlen(val) + 1];
+	std::strcpy(m_val, val);
+}
+
+char* Storage<char*>::GetVal() {
+	if(m_val == 0) {
+		throw std::range_error("Storage is empty");
+	}
+	char* val = new char[strlen(m_val) + 1];
+	std::strcpy(val, m_val);
+	return val;
+}
